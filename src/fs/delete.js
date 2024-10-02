259b09ffd77d7c63b093e498d23b5efa941cc6c5
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-03 01:53:42 +0200

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,31 @@
+// implement function that deletes file fileToRemove.txt (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+
+import fs from "fs/promises";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const sourceFile = path.join(
+    import.meta.dirname,
+    "files",
+    "fileToRemove.txt",
+  );
+
+  const isSourceFileExists = await fs
+    .access(sourceFile)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!isSourceFileExists) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.rm(sourceFile);
+    console.log("File removed successfully");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 await remove();
