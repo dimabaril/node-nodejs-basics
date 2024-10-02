0a05fdeb475166d3046e8fcd051dac46dcd037ec
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-03 01:14:16 +0200

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from "fs/promises";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const sourceFolder = path.join(import.meta.dirname, "files");
+  const destinationFolder = path.join(import.meta.dirname, "files_copy");
+
+  const [isSourceFolderExists, isDestinationFolderExists] = await Promise.all([
+    fs
+      .access(sourceFolder)
+      .then(() => true)
+      .catch(() => false),
+    fs
+      .access(destinationFolder)
+      .then(() => true)
+      .catch(() => false),
+  ]);
+
+  if (!isSourceFolderExists || isDestinationFolderExists) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.cp(sourceFolder, destinationFolder, { recursive: true });
+    console.log("Directory copied successfully");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 await copy();
