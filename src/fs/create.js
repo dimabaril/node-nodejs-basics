3dc9dd0c3d291d1ea9c530a5cd906ca596c68f10
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-03 00:45:16 +0200

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const filePath = path.join(import.meta.dirname, "files", "fresh.txt");
+
+  const isFileExists = await fs
+    .access(filePath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (isFileExists) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.writeFile(filePath, content);
+    console.log("File created successfully");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 await create();
