8b6a9691eb1adfb4ca0cb94a3651a97948e99286
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-03 02:44:42 +0200

    feat: add task comment above implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,6 @@
+// implement function that creates new file fresh.txt with content I am fresh and young
+// inside of the files folder (if file already exists Error with message FS operation failed must be thrown)
+
 import fs from "fs/promises";
 import path from "path";
 
