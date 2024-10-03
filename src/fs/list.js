c857467c915453c0c277aa911fa5f10f00443f2b
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-03 02:01:50 +0200

    feat: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+// implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)
+
+import fs from "fs/promises";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  const sourceFolder = path.join(import.meta.dirname, "files");
+
+  const isSourceFolderExists = await fs
+    .access(sourceFolder)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!isSourceFolderExists) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    const files = await fs.readdir(sourceFolder, { recursive: true });
+    console.log(files);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 await list();
