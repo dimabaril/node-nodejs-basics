9431c75de8bc0de4dbe3ef0e54613868c7e13455
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-04 23:18:49 +0200

    feat: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+// implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+
+import { createWriteStream } from "fs";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(
+    path.join(import.meta.dirname, "files", "fileToWrite.txt"),
+  );
+  //   process.stdin.on("data", (data) => {
+  //     stream.write(data);
+  //   });
+  process.stdin.pipe(stream);
+  process.stdin.on("error", (err) => {
+    console.error(err);
+  });
 };
 
 await write();
