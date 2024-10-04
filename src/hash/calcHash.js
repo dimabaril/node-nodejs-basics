752da1490e97a3ca033a0f763b09c4c5951a3727
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-04 18:10:53 +0200

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,34 @@
+// implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API
+
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const filePath = path.join(
+    import.meta.dirname,
+    "files",
+    "fileToCalculateHashFor.txt",
+  );
+
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(filePath);
+
+    stream.on("data", (data) => {
+      hash.update(data);
+    });
+
+    stream.on("end", () => {
+      const result = hash.digest("hex");
+      console.log(result);
+      resolve(result);
+    });
+
+    stream.on("error", (err) => {
+      reject(err);
+    });
+  });
 };
 
 await calculateHash();
