8e6855d58a2132615f6bae7f0ed78015d90d4244
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-05 00:53:55 +0200

    feat: decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+// implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import zlib from "zlib";
+import path from "path";
+
 const decompress = async () => {
-    // Write your code here 
+  const sourceFile = path.join(import.meta.dirname, "files", "archive.gz");
+  const destinationFile = path.join(
+    import.meta.dirname,
+    "files",
+    "fileToCompress.txt",
+  );
+  const readStream = createReadStream(sourceFile);
+  const writeStream = createWriteStream(destinationFile);
+
+  pipeline(readStream, zlib.createGunzip(), writeStream, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 await decompress();
