3992ad58dfe6e0d7f2efb5fa228fcba4c8ed4457
Dmitrii Barilkin <dimabaril@gmail.com>
2024-10-05 21:15:50 +0200

    feat: worker.js main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+// implement function that creates number of worker threads (equal to the number of host machine logical CPU cores) from file worker.js and able to send data to those threads and to receive result of the computation from them. You should send incremental number starting from 10 to each worker. For example: on host machine with 4 cores you should create 4 workers and send 10 to first worker, 11 to second worker, 12 to third worker, 13 to fourth worker. After all workers will finish, function should log array of results into console. The results are array of objects with 2 properties:
+// status - 'resolved' in case of successfully received value from worker or 'error' in case of error in worker
+// data - value from worker in case of success or null in case of error in worker
+
+import os from "os";
+import { Worker } from "worker_threads";
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpus = os.cpus().length;
+  const workers = [];
+  const results = [];
+  for (let i = 0; i < cpus; i++) {
+    workers.push(new Worker(path.resolve(import.meta.dirname, "worker.js")));
+  }
+  workers.forEach((worker, index) => {
+    worker.postMessage(index + 10);
+    worker.on("message", (data) => {
+      results.push(data);
+      if (results.length === cpus) {
+        console.log(results);
+      }
+    });
+    worker.on("error", (error) => {
+      results.push({ status: "error", data: null });
+      if (results.length === cpus) {
+        console.log(results);
+      }
+    });
+  });
 };
 
 await performCalculations();
